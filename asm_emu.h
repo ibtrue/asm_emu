4629710d58e43986022439a6cd484b7335f5d8dd
leshicorn <leshicorn@gmail.com>
2022-02-11 01:37:11 +0300

    asm_emu.h: added all functions from '...'.c

--- asm_emu.h
+++ asm_emu.h
@@ -27,12 +27,18 @@ long int    HEXtoDEC(char* str);
 int         charcmp(char s1, char s2);
 char**      split(char* str);
 //code functions
-void return_c(int code); // 99
+void return_c(); // 99
 void equating(data* vars, int ind_where, int ind_who); // 00
 void sum(data* vars, int arg1, int arg2, int res); // 01
 void subtraction(data* vars, int arg1, int arg2, int res); //02
 void multy(data* vars, int arg1, int arg2, int res); //03
 void div(data* vars, int arg1, int arg2, int res); //04
 void mod(data* vars, int arg1, int arg2, int res); //14
+int compeq(data* vars, int arg1, int arg2, int res);
+int compneq(data* vars, int arg1, int arg2, int res);
+int compl(data* vars, int arg1, int arg2, int res);
+int comple(data* vars, int arg1, int arg2, int res);
+int comph(data* vars, int arg1, int arg2, int res);
+int comphe(data* vars, int arg1, int arg2, int res);
 
 #endif
